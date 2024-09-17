a121414d8323564de6cafcd5917178990d9008be
17light <tadasaket@gmail.com>
2024-09-17 10:17:50 +0700

    first commit

---
+++ myapp/tailwind.config.js
@@ -0,0 +1,15 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sarabun: ["Sarabun"],
+        mitr: ["Mitr"],
+        promp: ["Prompt"]
+      }
+    },
+  },
+  plugins: [],
+}
+
